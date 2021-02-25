998efa95fb18ac4da7db4791a70d2bb7248a2a1f
Alex Basson <alexbasson@gmail.com>
2021-02-24 22:56:15 -0500

    Add error messaging if required values are missing

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const { PactConverter } = require('./src/PactConverter');
 const { WireMockStubWriter } = require('./src/WireMockStubWriter');
 
 program
-  .option('-p, --pathToPact <pathToPact>', 'The path to the pact file')
-  .option('-m, --mappingsDir <mappingsDir>', 'The directory to save the WireMock stubs to')
+  .option('-p, --pathToPact <pathToPact>', 'The path to the pact file; can be provided by PATH_TO_PACT env variable')
+  .option('-m, --mappingsDir <mappingsDir>', 'The directory to save the WireMock stubs to; can be provided by MAPPINGS_DIR env variable')
   .parse(process.argv);
 
-const convertPact = ({pathToPact, mappingsDir}) => {
+const convertPact = (pathToPact, mappingsDir) => {
   const pactConverter = PactConverter(pathToPact);
   const stubWriter = WireMockStubWriter(mappingsDir);
 
@@ -21,4 +21,18 @@ const convertPact = ({pathToPact, mappingsDir}) => {
   });
 }
 
-convertPact(program.opts());
+const options = program.opts();
+const pathToPact = options.pathToPact || process.env.PATH_TO_PACT
+const mappingsDir = options.mappingsDir || process.env.MAPPINGS_DIR
+
+let errorMessage = 'Missing the following required values:\n'
+errorMessage += pathToPact ? '' : '\t- the path to the pact file\n';
+errorMessage += mappingsDir ? '' : '\t- the WireMock mappings dir\n';
+errorMessage += 'The required values can be provided as command line arguments or as environment variables.\n'
+errorMessage += 'Run `pact2wiremock -h` for details.\n';
+
+if (!pathToPact || !mappingsDir) {
+  throw new Error(errorMessage);
+}
+
+convertPact(pathToPact, mappingsDir);
