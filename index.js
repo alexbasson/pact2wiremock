3eed0d048b93650690ca8f86f282feec6eb50f28
Alex Basson <alexbasson@gmail.com>
2021-02-24 23:14:22 -0500

    Extract convertPact to its own file

--- index.js
+++ index.js
@@ -1,37 +1,29 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const { PactConverter } = require('./src/PactConverter');
-const { WireMockStubWriter } = require('./src/WireMockStubWriter');
+const { convertPact } = require('./src/convertPact');
 
 program
-  .option('-p, --pathToPact <pathToPact>', 'The path to the pact file; can be provided by PATH_TO_PACT env variable')
-  .option('-m, --mappingsDir <mappingsDir>', 'The directory to save the WireMock stubs to; can be provided by MAPPINGS_DIR env variable')
+  .option(
+    '-p, --path-to-pact <type>',
+    'The path to the pact file; can be provided by PATH_TO_PACT env variable',
+    process.env.PATH_TO_PACT
+  )
+  .option(
+    '-m, --mappings-dir <type>',
+    'The directory to save the WireMock stubs to; can be provided by MAPPINGS_DIR env variable',
+    process.env.MAPPINGS_DIR
+  )
   .parse(process.argv);
 
-const convertPact = (pathToPact, mappingsDir) => {
-  const pactConverter = PactConverter(pathToPact);
-  const stubWriter = WireMockStubWriter(mappingsDir);
-
-  stubWriter.cleanStubDir();
-
-  pactConverter.toWireMockStubs((err, stubs) => {
-    if (err) throw err;
-    stubs.forEach(stubWriter.writeStubToFile);
-  });
-}
-
-const options = program.opts();
-const pathToPact = options.pathToPact || process.env.PATH_TO_PACT
-const mappingsDir = options.mappingsDir || process.env.MAPPINGS_DIR
-
-let errorMessage = 'Missing the following required values:\n'
-errorMessage += pathToPact ? '' : '\t- the path to the pact file\n';
-errorMessage += mappingsDir ? '' : '\t- the WireMock mappings dir\n';
-errorMessage += 'The required values can be provided as command line arguments or as environment variables.\n'
-errorMessage += 'Run `pact2wiremock -h` for details.\n';
+const {pathToPact, mappingsDir} = program.opts();
 
 if (!pathToPact || !mappingsDir) {
+  let errorMessage = 'Missing the following required values:\n'
+  errorMessage += pathToPact ? '' : '\t- the path to the pact file\n';
+  errorMessage += mappingsDir ? '' : '\t- the WireMock mappings dir\n';
+  errorMessage += 'The required values can be provided as command line arguments or as environment variables.\n'
+  errorMessage += 'Run `pact2wiremock -h` for details.\n';
   throw new Error(errorMessage);
 }
 
---
+++ src/convertPact.js
@@ -0,0 +1,18 @@
+const { PactConverter } = require('./PactConverter');
+const { WireMockStubWriter } = require('./WireMockStubWriter');
+
+const convertPact = (pathToPact, mappingsDir) => {
+  const pactConverter = PactConverter(pathToPact);
+  const stubWriter = WireMockStubWriter(mappingsDir);
+
+  stubWriter.cleanStubDir();
+
+  pactConverter.toWireMockStubs((err, stubs) => {
+    if (err) throw err;
+    stubs.forEach(stubWriter.writeStubToFile);
+  });
+};
+
+module.exports = {
+  convertPact
+};
